0149a5fcd3642dd14718478e870fb1ce95848d98
Pedro Feitosa <pedr.fts@gmail.com>
2023-04-17 15:25:01 -0300

    hide e organizado as notification

---
+++ templates/static/usuarios/js/hide.js
@@ -0,0 +1,13 @@
+const eyeIcons = document.querySelectorAll(".show-hide");
+
+eyeIcons.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    const pInput = eyeIcon.parentElement.querySelector("input"); //getting parent element of eye icon and selecting the password input
+    if (pInput.type === "password") {
+      eyeIcon.classList.replace("bx-hide", "bx-show");
+      return (pInput.type = "text");
+    }
+    eyeIcon.classList.replace("bx-show", "bx-hide");
+    pInput.type = "password";
+  });
+});
